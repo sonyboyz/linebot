d9fe8c057b98b755c63d848258d9bc40e385d5c4
sonyboyz <sonyboyz@gmail.com>
2020-03-30 14:16:40 +0700

    Update bot-flex.php

--- bot-flex.php
+++ bot-flex.php
@@ -87,48 +87,132 @@ $jsonFlex = [
           ],
           [
             "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "New balance",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "฿ 45.57",
-                "align" => "end"
-              ]
-            ]
-          ],
-          [
-            "type" => "separator",
-            "margin" => "lg",
-            "color" => "#C3C3C3"
-          ]
+[
+  "type" => "flex",
+  "altText" => "Flex Message",
+  "contents" => [
+    "type" => "bubble",
+    "direction" => "ltr",
+    "header" => [
+      "type" => "box",
+      "layout" => "vertical",
+      "contents" => [
+        [
+          "type" => "text",
+          "text" => "ระบบรายงานวันลา",
+          "margin" => "xs",
+          "size" => "lg",
+          "align" => "center",
+          "gravity" => "bottom",
+          "weight" => "bold",
+          "color" => "#170574",
+          "wrap" => true
+        ],
+        [
+          "type" => "text",
+          "text" => "นายวิวัฒน์ คล้ายหล่อ",
+          "size" => "lg",
+          "align" => "center",
+          "weight" => "bold",
+          "color" => "#DD2104"
+        ],
+        [
+          "type" => "separator",
+          "color" => "#3A3030"
         ]
-      ],
-      "footer" => [
-        "type" => "box",
-        "layout" => "horizontal",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "View Details",
-            "size" => "lg",
-            "align" => "start",
-            "color" => "#0084B6",
-            "action" => [
-              "type" => "uri",
-              "label" => "View Details",
-              "uri" => "https://google.co.th/"
+      ]
+    ],
+    "body" => [
+      "type" => "box",
+      "layout" => "vertical",
+      "flex" => 2,
+      "contents" => [
+        [
+          "type" => "box",
+          "layout" => "baseline",
+          "contents" => [
+            [
+              "type" => "text",
+              "text" => "ลาป่วย",
+              "weight" => "regular",
+              "color" => "#0D00B9"
+            ],
+            [
+              "type" => "text",
+              "text" => "คงเหลือ  15  วัน",
+              "align" => "end",
+              "color" => "#000000"
+            ]
+          ]
+        ],
+        [
+          "type" => "box",
+          "layout" => "baseline",
+          "contents" => [
+            [
+              "type" => "text",
+              "text" => "ลาพักผ่อน",
+              "color" => "#BB0D0D"
+            ],
+            [
+              "type" => "text",
+              "text" => "คงเหลือ 12 วัน",
+              "align" => "end"
+            ]
+          ]
+        ],
+        [
+          "type" => "box",
+          "layout" => "baseline",
+          "contents" => [
+            [
+              "type" => "text",
+              "text" => "ลากิจ",
+              "color" => "#11AA08"
+            ],
+            [
+              "type" => "text",
+              "text" => "คงเหลือ 5 วัน",
+              "align" => "end"
+            ]
+          ]
+        ],
+        [
+          "type" => "box",
+          "layout" => "baseline",
+          "contents" => [
+            [
+              "type" => "text",
+              "text" => "ลาคลอด/อุปสมบท",
+              "color" => "#1F262C"
+            ],
+            [
+              "type" => "text",
+              "text" => "คงเหลือ 20 วัน",
+              "align" => "end"
             ]
           ]
         ]
       ]
+    ],
+    "footer" => [
+      "type" => "box",
+      "layout" => "horizontal",
+      "contents" => [
+        [
+          "type" => "button",
+          "action" => [
+            "type" => "uri",
+            "label" => "ยื่นใบลา",
+            "uri" => "http://www.rh2.go.th"
+          ],
+          "color" => "#00BB51",
+          "style" => "primary"
+        ]
+      ]
     ]
-  ];
+  ]
+];
 
 
 
