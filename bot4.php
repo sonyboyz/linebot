a8f24fc1963b7beabd0c704ebc5b042ade8bb92f
sonyboyz <sonyboyz@gmail.com>
2020-03-30 13:42:11 +0700

    Update bot4.php

--- bot4.php
+++ bot4.php
@@ -15,7 +15,7 @@ $request_array = json_decode($request, true);   // Decode JSON to Array
 $jsonFlex = 
     {
   "type": "flex",
-  "altText": "Flex Message",
+  "altText": "รายงานวันลา",
   "contents": {
     "type": "bubble",
     "direction": "ltr",
