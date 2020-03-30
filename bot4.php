14bb91857a4e9c8af001c0fb303addd4f7ab5985
sonyboyz <sonyboyz@gmail.com>
2020-03-30 13:53:37 +0700

    Update bot4.php

--- bot4.php
+++ bot4.php
@@ -11,132 +11,131 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
 $jsonFlex = [
-  "type": "flex",
-  "altText": "รายงานวันลา",
-  "contents": {
-    "type": "bubble",
-    "direction": "ltr",
-    "header": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "text",
-          "text": "ระบบรายงานวันลา",
-          "margin": "xs",
-          "size": "lg",
-          "align": "center",
-          "gravity": "bottom",
-          "weight": "bold",
-          "color": "#170574",
-          "wrap": true
-        },
-        {
-          "type": "text",
-          "text": "นายวิวัฒน์ คล้ายหล่อ",
-          "size": "lg",
-          "align": "center",
-          "weight": "bold",
-          "color": "#DD2104"
-        },
-        {
-          "type": "separator",
-          "color": "#3A3030"
-        }
+  "type"=> "flex",
+  "altText"=> "รายงานวันลา",
+  "contents"=> [
+    "type"=> "bubble",
+    "direction"=> "ltr",
+    "header"=> [
+      "type"=> "box",
+      "layout"=> "vertical",
+      "contents"=> [
+        [
+          "type"=> "text",
+          "text"=> "ระบบรายงานวันลา",
+          "margin"=> "xs",
+          "size"=> "lg",
+          "align"=> "center",
+          "gravity"=> "bottom",
+          "weight"=> "bold",
+          "color"=> "#170574",
+          "wrap"=> true
+        ],
+        [
+          "type"=> "text",
+          "text"=> "นายวิวัฒน์ คล้ายหล่อ",
+          "size"=> "lg",
+          "align"=> "center",
+          "weight"=> "bold",
+          "color"=> "#DD2104"
+        ],
+        [
+          "type"=> "separator",
+          "color"=> "#3A3030"
+        ]
       ]
-    },
-    "body": {
-      "type": "box",
-      "layout": "vertical",
-      "flex": 2,
-      "contents": [
-        {
-          "type": "box",
-          "layout": "baseline",
-          "contents": [
-            {
-              "type": "text",
-              "text": "ลาป่วย",
-              "weight": "regular",
-              "color": "#0D00B9"
-            },
-            {
-              "type": "text",
-              "text": "คงเหลือ  15  วัน",
-              "align": "end",
-              "color": "#000000"
-            }
+    ],
+    "body"=> [
+      "type"=> "box",
+      "layout"=> "vertical",
+      "flex"=> 2,
+      "contents"=> [
+        [
+          "type"=> "box",
+          "layout"=> "baseline",
+          "contents"=> [
+            [
+              "type"=> "text",
+              "text"=> "ลาป่วย",
+              "weight"=> "regular",
+              "color"=> "#0D00B9"
+            ],
+            [
+              "type"=> "text",
+              "text"=> "คงเหลือ  15  วัน",
+              "align"=> "end",
+              "color"=> "#000000"
+            ]
           ]
-        },
-        {
-          "type": "box",
-          "layout": "baseline",
-          "contents": [
-            {
-              "type": "text",
-              "text": "ลาพักผ่อน",
-              "color": "#BB0D0D"
-            },
-            {
-              "type": "text",
-              "text": "คงเหลือ 12 วัน",
-              "align": "end"
-            }
+        ],
+        [
+          "type"=> "box",
+          "layout"=> "baseline",
+          "contents"=> [
+            [
+              "type"=> "text",
+              "text"=> "ลาพักผ่อน",
+              "color"=> "#BB0D0D"
+            ],
+            [
+              "type"=> "text",
+              "text"=> "คงเหลือ 12 วัน",
+              "align"=> "end"
+            ]
           ]
-        },
-        {
-          "type": "box",
-          "layout": "baseline",
-          "contents": [
-            {
-              "type": "text",
-              "text": "ลากิจ",
-              "color": "#11AA08"
-            },
-            {
-              "type": "text",
-              "text": "คงเหลือ 5 วัน",
-              "align": "end"
-            }
+        ],
+        [
+          "type"=> "box",
+          "layout"=> "baseline",
+          "contents"=> [
+            [
+              "type"=> "text",
+              "text"=> "ลากิจ",
+              "color"=> "#11AA08"
+            ],
+            [
+              "type"=> "text",
+              "text"=> "คงเหลือ 5 วัน",
+              "align"=> "end"
+            ]
           ]
-        },
-        {
-          "type": "box",
-          "layout": "baseline",
-          "contents": [
-            {
-              "type": "text",
-              "text": "ลาคลอด/อุปสมบท",
-              "color": "#1F262C"
-            },
-            {
-              "type": "text",
-              "text": "คงเหลือ 20 วัน",
-              "align": "end"
-            }
+        ],
+        [
+          "type"=> "box",
+          "layout"=> "baseline",
+          "contents"=> [
+            [
+              "type"=> "text",
+              "text"=> "ลาคลอด/อุปสมบท",
+              "color"=> "#1F262C"
+            ],
+            [
+              "type"=> "text",
+              "text"=> "คงเหลือ 20 วัน",
+              "align"=> "end"
+            ]
           ]
-        }
+        ]
       ]
-    },
-    "footer": {
-      "type": "box",
-      "layout": "horizontal",
-      "contents": [
-        {
-          "type": "button",
-          "action": {
-            "type": "uri",
-            "label": "ยื่นใบลา",
-            "uri": "http://www.rh2.go.th"
-          },
-          "color": "#00BB51",
-          "style": "primary"
-        }
+    ],
+    "footer"=> [
+      "type"=> "box",
+      "layout"=> "horizontal",
+      "contents"=> [
+        [
+          "type"=> "button",
+          "action"=> [
+            "type"=> "uri",
+            "label"=> "ยื่นใบลา",
+            "uri"=> "http=>//www.rh2.go.th"
+          ],
+          "color"=> "#00BB51",
+          "style"=> "primary"
+        ]
       ]
-    }
-  }
-]
-  ;
+    ]
+  ]
+];
 
 
 
