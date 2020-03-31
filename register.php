a01b041f362a11d1d7fce45258c1fa6a28eb243b
sonyboyz <sonyboyz@gmail.com>
2020-03-31 16:34:37 +0700

    Update register.php

--- register.php
+++ register.php
@@ -12,32 +12,7 @@
    #ตัวอย่าง Message Type "Text + Sticker"
 $nameRegis = substr($message,8);
 //Check DB
-  $servername = "203.157.118.122:3306";
-  $username = "root";
-  $password = "P-Triple1331";
-  $dbname = "profile_rh2";
-  $mysql = new mysqli($servername, $username, $password, $dbname);
-  mysqli_set_charset($mysql, "utf8");
-
-  if ($mysql->connect_error){
-  $errorcode = $mysql->connect_error;
-  print("MySQL(Connection)> ".$errorcode);
-  }
-
-  function sendMessage($replyJson, $sendInfo){
-          $ch = curl_init($sendInfo["URL"]);
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-          curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-          curl_setopt($ch, CURLOPT_POST, true);
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-              'Content-Type: application/json',
-              'Authorization: Bearer ' . $sendInfo["AccessToken"])
-              );
-          curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
-          $result = curl_exec($ch);
-          curl_close($ch);
-    return $result;
-  }
+ 
 
   
       $arrayPostData['to'] = $id;
