8da5066d780920db516cf44a6a9e592219fb0814
sonyboyz <sonyboyz@gmail.com>
2020-03-31 21:08:27 +0700

    Update register.php

--- register.php
+++ register.php
@@ -67,7 +67,7 @@ $nameRegis = substr($message,8);
 	  
 
 	  $mysql->query("INSERT INTO `log`(`id_person`, `Text`, `Timestamp`, `detail`) VALUES ('$userID','$text','$timestamp','Check Get Row')");
-	  
+	  include "bot_push.php"; 
 	  exit;
 	  //$replyText["text"] = "คุณ $p_name ลงทะเบียนไปแล้วครับ ^ ^";
    
